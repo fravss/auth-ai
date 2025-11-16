e9fac7cc08b3019b90695dbcb57b604c5445dc66
fravss <anaflaviamartins56@gmail.com>
2025-11-16 13:54:18 -0300

    ENVIRONMENT

--- src/main/java/com/chatbot/ai/core/security/SecurityConfigurations.java
+++ src/main/java/com/chatbot/ai/core/security/SecurityConfigurations.java
@@ -46,7 +46,7 @@ public class SecurityConfigurations {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://35.175.146.213:4200")); //ATUALIZAR
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
